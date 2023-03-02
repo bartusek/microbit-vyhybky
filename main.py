7b7015a84528d4c78b5d0c5093c8872197e1121e
bartusek <bartusek@users.noreply.github.com>
2023-03-02 23:15:15 +0100

    pridane ovladani tlacitkem

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 def prehodVyhybku(num: number):
     if stavy_vyhybek[num]:
-        stavy_vyhybek[num]=0
+        stavy_vyhybek[num] = 0
         prehodServo(num, 90)
     else:
-        stavy_vyhybek[num]=1
+        stavy_vyhybek[num] = 1
         prehodServo(num, 180)
-
+    return stavy_vyhybek[num]
+def dejCisloTlacitka(hodnota_pinu2: number):
+    if hodnota_pinu2 < 20:
+        return 1
+    elif hodnota_pinu2 < 70:
+        return 2
+    elif hodnota_pinu2 < 110:
+        return 3
+    elif hodnota_pinu2 < 150:
+        return 4
+    elif hodnota_pinu2 < 600:
+        return 5
+    else:
+        return 0
+def vytvorObrazek(tlacitko: number, pozice_vyhybky: number):
+    basic.clear_screen()
+    if tlacitko > 0:
+        led.plot(tlacitko - 1, 0)
+    if pozice_vyhybky == 0:
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.plot(3, 2)
+        led.plot(4, 0)
+        led.plot(4, 1)
+    elif pozice_vyhybky == 1:
+        led.plot(4, 0)
+        led.plot(4, 1)
+        led.plot(4, 2)
+        led.plot(4, 3)
+        led.plot(4, 4)
 def prehodServo(num2: number, angle: number):
     if num2 == 0:
         wuKong.set_servo_angle(wuKong.ServoTypeList._270, wuKong.ServoList.S0, angle)
@@ -23,8 +52,19 @@ def prehodServo(num2: number, angle: number):
         wuKong.set_servo_angle(wuKong.ServoTypeList._270, wuKong.ServoList.S6, angle)
     elif num2 == 7:
         wuKong.set_servo_angle(wuKong.ServoTypeList._270, wuKong.ServoList.S7, angle)
+def tlacitkoZmacknute(hodnota_pinu: number):
+    return hodnota_pinu < 600
+pozice_vyhybky2 = 0
+tlacitko2 = 0
+stavy_vyhybek: List[number] = []
 stavy_vyhybek = [0, 0, 0, 0, 0, 0, 0]
 
 def on_forever():
-    basic.show_number(pins.analog_read_pin(AnalogPin.P0))
+    global tlacitko2, pozice_vyhybky2
+    tlacitko2 = dejCisloTlacitka(pins.analog_read_pin(AnalogPin.P1))
+    basic.clear_screen()
+    pozice_vyhybky2 = 1000
+    if tlacitko2 == 3:
+        prehodVyhybku(1)
+        vytvorObrazek(tlacitko2, pozice_vyhybky2)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -6,6 +6,41 @@ function prehodVyhybku (num: number) {
         stavy_vyhybek[num] = 1
         prehodServo(num, 180)
     }
+    return stavy_vyhybek[num]
+}
+function dejCisloTlacitka (hodnota_pinu2: number) {
+    if (hodnota_pinu2 < 20) {
+        return 1
+    } else if (hodnota_pinu2 < 70) {
+        return 2
+    } else if (hodnota_pinu2 < 110) {
+        return 3
+    } else if (hodnota_pinu2 < 150) {
+        return 4
+    } else if (hodnota_pinu2 < 600) {
+        return 5
+    } else {
+        return 0
+    }
+}
+function vytvorObrazek (tlacitko: number, pozice_vyhybky: number) {
+    basic.clearScreen()
+    if (tlacitko > 0) {
+        led.plot(tlacitko - 1, 0)
+    }
+    if (pozice_vyhybky == 0) {
+        led.plot(2, 3)
+        led.plot(2, 4)
+        led.plot(3, 2)
+        led.plot(4, 0)
+        led.plot(4, 1)
+    } else if (pozice_vyhybky == 1) {
+        led.plot(4, 0)
+        led.plot(4, 1)
+        led.plot(4, 2)
+        led.plot(4, 3)
+        led.plot(4, 4)
+    }
 }
 function prehodServo (num2: number, angle: number) {
     if (num2 == 0) {
@@ -26,6 +61,11 @@ function prehodServo (num2: number, angle: number) {
         wuKong.setServoAngle(wuKong.ServoTypeList._270, wuKong.ServoList.S7, angle)
     }
 }
+function tlacitkoZmacknute (hodnota_pinu: number) {
+    return hodnota_pinu < 600
+}
+let pozice_vyhybky2 = 0
+let tlacitko2 = 0
 let stavy_vyhybek: number[] = []
 stavy_vyhybek = [
 0,
@@ -36,6 +76,13 @@ stavy_vyhybek = [
 0,
 0
 ]
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-    basic.showNumber(pins.analogReadPin(AnalogPin.P0))
+    tlacitko2 = dejCisloTlacitka(pins.analogReadPin(AnalogPin.P1))
+    basic.clearScreen()
+    pozice_vyhybky2 = 1000
+    if (tlacitko2 == 3) {
+        prehodVyhybku(1)
+        vytvorObrazek(tlacitko2, pozice_vyhybky2)
+    }
 })
